eba51043460f4071348f2ddde517cda40dc4ee9e
Akss007 <akshansh.sharma96@gmail.com>
2021-01-11 22:14:40 +0530

    Crispy Form Styling, Email setup

--- Assignment_Tool/Assignment_Tool/settings.py
+++ Assignment_Tool/Assignment_Tool/settings.py
@@ -158,13 +158,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGOUT_REDIRECT_URL = '/'
 
 
-if not DEBUG:
+if DEBUG:
       EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-      EMAIL_HOST_USER = "parwizforogh0@gmail.com"
+      EMAIL_HOST_USER = "technopreneurs123@gmail.com"
       EMAIL_HOST = 'smtp.gmail.com'
       EMAIL_PORT = 587
       EMAIL_USE_TLS = True
-      EMAIL_HOST_PASSWORD = "Your Password"
+      EMAIL_HOST_PASSWORD = get_secret('EMAIL_PASSWORD')
 
 else:
     EMAIL_BACKEND = (
